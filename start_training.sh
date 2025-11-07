05df76b88b3ed9ec4fb661fbb6b519d55028c902
TidosDK <git@msnn.dev>
2025-11-07 14:17:48 +0100

    Add a start training script

---
+++ start_training.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+python3 -m venv venv
+source venv/bin/activate
+
+sudo apt update
+sudo apt install -y libgl1 -y
+sudo apt install -y swig build-essential python3-dev -y
+
+pip install -r requirements.txt
+
+python3 train_multiprocess.py
+
+echo "Finished training"
