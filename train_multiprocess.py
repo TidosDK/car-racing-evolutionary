5242523a23825370b36e4c9ed395db17b5e60993
TidosDK <git@msnn.dev>
2025-12-15 11:39:27 +0100

    Fixed error where archive would not be set for fitness-only training

--- train_multiprocess.py
+++ train_multiprocess.py
@@ -203,7 +203,7 @@ class neat_algorithm:
 		if mode.lower() == "novelty":
 			archive = NoveltyArchive(threshold=5.0, k_neighbors=15)
 
-			self.reporter.set_archive(archive)
+		self.reporter.set_archive(archive)
 
 		evaluator = CustomEvaluator(
 			num_workers=WORKERS,
